7737045c3cf18c8267251fee503603603bb05d77
Sachin Iyer <sachinjiyer@gmail.com>
2023-01-27 03:27:21 -0500

    fix rebase with main as well

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -2,8 +2,6 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint structs2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Order {
     name: String,
@@ -27,6 +25,18 @@ fn create_order_template() -> Order {
     }
 }
 
+fn your_order_template() -> Order {
+    Order {
+        name: String::from("Hacker in Rust"),
+        year: 2019,
+        made_by_phone: false,
+        made_by_mobile: false,
+        made_by_email: true,
+        item_number: 123,
+        count: 1,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -34,8 +44,7 @@ mod tests {
     #[test]
     fn your_order() {
         let order_template = create_order_template();
-        // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = your_order_template();
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
